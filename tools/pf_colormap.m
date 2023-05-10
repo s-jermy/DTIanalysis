b25ff7254f2ba909624a2cca7c5f905d64c079d2
liztunnicliffe <github@plumlake.org>
2023-05-10 10:45:27 +0100

    pf_colormap.m now cross-platform friendly

--- tools/pf_colormap.m
+++ tools/pf_colormap.m
@@ -1,6 +1,6 @@
 function CM = pf_colormap(map)
 
-clr_folder = 'tools\cardiac_DTI_colormaps-master\colormaps_data';
+clr_folder = fullfile('tools','cardiac_DTI_colormaps-master','colormaps_data');
 clr_name = [map '.txt'];
 try
     CM = load(fullfile(clr_folder,clr_name));
