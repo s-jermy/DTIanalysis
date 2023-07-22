cdcc3069409deba657c0563d2185db0d1e1a15ec
s-jermy <stephen.jermy@uct.ac.za>
2023-07-22 20:58:28 +0200

    Update SaveGlyphs.m
    Automatically position camera, the chosen values were based on trial and error for what looked best

--- SaveGlyphs.m
+++ SaveGlyphs.m
@@ -7,6 +7,15 @@ hf = figures.hfig;
 for i=1:length(hf)
     f = hf{i}.Tag;
     n = hf{i}.Name;
+    ud = hf{i}.UserData;
+
+    targetx = round(ud(2,1));
+    targety = round(ud(3,2));
+
+    view(-12,16);
+    campos([-76,-633,202]);
+    camtarget([targetx,targety,0]);
+    camva(2.3);
 
     saveas(hf{i},fullfile(saveDir,f,n));
 end
