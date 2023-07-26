26a046e5e7e1bd02eedc3052e1b1d5d31a1a12e3
s-jermy <stephen.jermy@uct.ac.za>
2023-07-26 10:23:17 +0200

    Update SaveGlyphs.m
    Camera changes now apply to all axes of a figure

--- SaveGlyphs.m
+++ SaveGlyphs.m
@@ -16,19 +16,21 @@ for i=1:length(hf) %cardiac phases and slice locations
         op = hf{i}{j}.OuterPosition; %sj - store old figure position and size
         hf{i}{j}.OuterPosition = [0 0 800 800]; %sj - change to standard size
     
-        view(-12,16);
-        campos([-76,-633,202]);
-        camva(2.3);
+        %move camera to low angle view
+        set(gcf().Children,'View',[-12 16])
+        set(gcf().Children,'CameraPosition',[-76 633 202])
+        set(gcf().Children,'CameraViewAngle',2.3)
     
         fname1 = fullfile(saveDir,f,'glyph');
         warning('off','MATLAB:MKDIR:DirectoryExists');
         mkdir(fname1);
         warning('on','MATLAB:MKDIR:DirectoryExists');
     
+        %sj - save a couple different views
         for x = 1:length(targetx)
-            camtarget([targetx(x),targety(3),0]);
+            set(gcf().Children,'CameraTarget',[targetx(x) targety(3) 0])
             fname = fullfile(fname1,[n '_' int2str(x) '.png']);
-            export_fig(fname, '-png', '-transparent', '-r100'); %sj - save a couple different views
+            export_fig(fname, '-png', '-transparent', '-r100');
         end
     
         hf{i}{j}.OuterPosition = op; %sj - reset back to original
