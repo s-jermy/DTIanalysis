69d353eacfa21272f0bebf8cf6a433760fc0f164
Stephen <stephen.jermy@uct.ac.za>
2025-07-02 11:55:00 +0200

    Update ChooseFolder.m
    Update to folder names

--- ChooseFolder.m
+++ ChooseFolder.m
@@ -97,7 +97,7 @@ switch tag
             fullfile('Oxford','20180629_O3TPR_C11-01_10286','BH'),...	    #9
             fullfile('Oxford','20180629_O3TPR_C11-01_10286','Gate'),...	    #10
             fullfile('Oxford','20180629_O3TPR_C11-01_10286','Nav'),...	    #11
-            fullfile('Oxford','20180629_O3TPR_C11-01_10286','CS')...	    #12
+            fullfile('Oxford','20180629_O3TPR_C11-01_10286','CS')...        #12
             };
     case 'steve_oxford_2020'
         files={
@@ -135,17 +135,17 @@ switch tag
             };
     case 'steve_oxford_2025'
         files={
-            fullfile('Oxford','20250226_O3TPR_C21-06_26200','BH2RR'),...    #1
-            fullfile('Oxford','20250226_O3TPR_C21-06_26200','CS2RR'),...    #2
-            fullfile('Oxford','20250226_O3TPR_C21-06_26200','FB2RR'),...    #3
-            fullfile('Oxford','20250602_O3TPR_CD01_26437','BH'),...         #4
-            fullfile('Oxford','20250602_O3TPR_CD01_26437','Nav'),...        #5
-            fullfile('Oxford','20250603_O3TPR_CD01_26440','BH'),...         #6
-            fullfile('Oxford','20250603_O3TPR_CD01_26440','Gate'),...       #7
-            fullfile('Oxford','20250603_O3TPR_CD01_26440','Nav'),...        #8
-            fullfile('Oxford','20250603_O3TPR_CD01_26443','BH'),...         #9
-            fullfile('Oxford','20250603_O3TPR_CD01_26443','Gate'),...       #10
-            fullfile('Oxford','20250603_O3TPR_CD01_26443','Nav'),...        #11
+            fullfile('Oxford','20250226_O3TPR_C21-06_26200','BH'),...   #1 2RR
+            fullfile('Oxford','20250226_O3TPR_C21-06_26200','CS'),...   #2
+            fullfile('Oxford','20250226_O3TPR_C21-06_26200','FB'),...   #3 2RR
+            fullfile('Oxford','20250602_O3TPR_CD01_26437','BH'),...     #4 2RR
+            fullfile('Oxford','20250602_O3TPR_CD01_26437','Nav'),...    #5 2RR
+            fullfile('Oxford','20250603_O3TPR_CD01_26440','BH'),...     #6 2RR
+            fullfile('Oxford','20250603_O3TPR_CD01_26440','Gate'),...   #7 2RR
+            fullfile('Oxford','20250603_O3TPR_CD01_26440','Nav'),...    #8 2RR
+            fullfile('Oxford','20250603_O3TPR_CD01_26443','BH'),...     #9 2RR
+            fullfile('Oxford','20250603_O3TPR_CD01_26443','Gate'),...   #10 2RR
+            fullfile('Oxford','20250603_O3TPR_CD01_26443','Nav'),...    #11 2RR
             };
     case 'zak'
         files={
